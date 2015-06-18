ae41059d83bfd7828ea8d71f930e12ac8258d99d
blxd <blxd@users.noreply.github.com>
2015-06-18 14:27:50 +0000

    updated script to support new tvplayer.com website

--- src/livestreamer/plugins/tvplayer.py
+++ src/livestreamer/plugins/tvplayer.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
+import hashlib
+import random
 import re
+from urllib import unquote
 
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
-from livestreamer.stream import HLSStream
+from stream import HLSStream
 
-USER_AGENT_STRING = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) "
-                     "AppleWebKit/537.36 (KHTML, like Gecko) "
-                     "Chrome/43.0.2357.65 Safari/537.36")
-STREAM_INFO_URL = "http://lapi.cdn.tvplayer.com/tvplayer/stream/live/id/{id}"
+TVPLAYER_STREAM_WEB_ENCRYPTED_URL = "http://live.tvplayer.com/stream-web-encrypted.php"
+TVPLAYER_REFERER = ("http://assets.storage.uk.tvplayer.com.s3.amazonaws.com/web/"
+                    "flash/secure_SSMPlayer_4-8-4.swf?nocache={0}".format(random.randint(1, 20000)))
+FLASH_VERSION_STRING = "ShockwaveFlash/18.0.0.161"
+TVPLAYER_ORIGIN = "http://assets.storage.uk.tvplayer.com.s3.amazonaws.com"
+TVPLAYER_SECRET_KEY = "KADI4591AKCLLE"
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+
 _url_re = re.compile(r"http://(?:www.)?tvplayer.com/watch/(.+)")
-_channel_map_re = re.compile(r'href="/watch/([a-z]+?)".*?img.*?src=".*?/(\d+).png"', re.S)
+_hash_var = re.compile(r"""var hash="(.*)";""")
+_exp_var = re.compile(r"""var exp="(\d+)";""")
+_channel_map_re = re.compile(r"""/Colour/(\d+).png""")
+
 _channel_schema = validate.Schema({
-    "stream": validate.url(scheme=validate.any("http"))
+    "tvplayer":
+        {u'endpoint': u'stream',
+         u'response':
+             {u'cc': bool,
+              u'stream': validate.url(scheme=validate.any("http"))},
+         u'time': unicode}
 })
 
 
@@ -27,17 +42,37 @@ class TVPlayer(Plugin):
         if url_match:
             # find the list of channels from the html in the page
             res = http.get(self.url)
-            channel_map = dict(_channel_map_re.findall(res.text))
-            channel_id = channel_map.get(url_match.group(1))
+            channel_map = _channel_map_re.search(res.text)
+            if channel_map:
+                channel_id = channel_map.group(1)
 
-            # get the stream urls
-            res = http.get(STREAM_INFO_URL.format(id=channel_id))
-            stream_data = http.json(res, schema=_channel_schema)
+                # get the token_expiry and hash vars from the html page
+                try:
+                    var_token_expiry = _exp_var.findall(res.text)[0]
+                    var_hash = unquote(_hash_var.findall(res.text)[0])
+                except IndexError:
+                    # page layout changed
+                    raise
 
-            return HLSStream.parse_variant_playlist(self.session,
-                                                    stream_data['stream'],
-                                                    headers={'user-agent': USER_AGENT_STRING})
+                # compute the key for the request
+                key = hashlib.md5(var_token_expiry + TVPLAYER_SECRET_KEY).hexdigest()
+
+                headers = {
+                    "Hash": var_hash,
+                    "Token-Expiry": var_token_expiry,
+                    "Key": key,
+                    "User-Agent": USER_AGENT,
+                    "Referer": TVPLAYER_REFERER,
+                    "X-Requested-With": FLASH_VERSION_STRING,
+                    "Origin": TVPLAYER_ORIGIN
+                }
+
+                res = http.post(TVPLAYER_STREAM_WEB_ENCRYPTED_URL,
+                                data=dict(id=channel_id),
+                                headers=headers)
+                stream_data = http.json(res, schema=_channel_schema)
+                http.cookies.update(res.cookies)
+                return HLSStream.parse_variant_playlist(self.session, stream_data['tvplayer']['response']['stream'])
 
 
 __plugin__ = TVPlayer
-
